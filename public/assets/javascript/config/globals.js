b9d985093590097ce1ed4eef2ffdc0e61fcd5de0
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-04 14:50:23 +0100

    1) risolto valori variabili globali della pagina homepage photoblog 2) photoblog funzionante con le api create (poche modifiche, modularizzazione codice efficace)

--- public/assets/javascript/config/globals.js
+++ public/assets/javascript/config/globals.js
@@ -1,8 +1,8 @@
 export const dataSaved = [];
-export const _URL = "http://localhost:3000/";
-export const _RESOURCE = "posts";
-export const _KEY1 = "_limit";
-export const _VALUE1 = 6;
+export const _URL = "http://localhost:3000";
+export const _RESOURCE = "/posts";
+export const _KEY1 = "";
+export const _VALUE1 = "";
 export const _PARAMS = {
     [_KEY1]: isNaN(_VALUE1) || _VALUE1 < 0 ? 0 : _VALUE1,
 };
